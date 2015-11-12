9f16bb64fa55234bbc485bdc302be8549f91cd43
alex322 <alex.esaulov@inbox.ru>
2015-11-12 22:21:37 +0300

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,4 +1,4 @@
 <?php
-
+//
     Route::get('_tattler', 'Grohman\Tattler\Controllers\SettingsController@getIndex');
     Route::post('_tattler', 'Grohman\Tattler\Controllers\SettingsController@postIndex');
